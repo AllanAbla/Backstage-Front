00cebb97e4c88a0210adf6025969aadcdfc1ddcd
Allan Abla <abla.allan999@gmail.com>
2025-12-08 18:36:31 -0300

    Alterado cor principal de azul para vermelho

--- src/pages/MapGL.jsx
+++ src/pages/MapGL.jsx
@@ -92,7 +92,7 @@ function addMarker(map, point) {
   el.className = "theater-marker";
   el.style.cssText = `
     width: 10px; height: 10px; border-radius: 50%;
-    background: #4aa8ff;
+    background: #C62828;
     box-shadow: 0 0 0 3px rgba(74,168,255,.25);
     border: 1px solid rgba(0,0,0,.2);
     cursor: pointer;
